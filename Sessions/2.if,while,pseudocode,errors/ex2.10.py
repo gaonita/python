a92ad452eb927e4178e07de72782e1ed170b02ba
Gaon <gaon.yg@gmail.com>
2018-08-20 20:40:36 +0200

    initial

---
+++ Sessions/2.if,while,pseudocode,errors/ex2.10.py
@@ -0,0 +1,10 @@
+i = 0
+y = i ** 2
+
+while y < 200:
+    i += 1
+   # print(i)
+    y += i ** 2
+    print(y)
+#print(y)
+#print(i)
---
+++ Sessions/2.if,while,pseudocode,errors/ex2.15.py
@@ -0,0 +1,8 @@
+import numpy
+
+dividend = float(input("Please enter the dividend:"))
+divisor = float(input("Please enter the divisor:"))
+quotient = dividend / divisor
+print(quotient)
+quotient_rounded = round(quotient)
+print(quotient_rounded)
